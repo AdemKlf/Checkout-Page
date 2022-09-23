26de8d0a7820f6c238b18788994811c4a5a25540
adem kalfalı <ademkalfa9@gmail.com>
2022-09-23 14:08:18 +0300

    Local/Session Storage Add

---
+++ app.js
@@ -0,0 +1,15 @@
+const taxRate = 0.18;
+const shippingPrice = 15;
+const shippingFreePrice = 300;
+
+window.addEventListener("load", () => {
+  //set items to LocalStorage
+  localStorage.setItem("taxRate", taxRate);
+  localStorage.setItem("shippingPrice", shippingPrice);
+  localStorage.setItem("shippingFreePrice", shippingFreePrice);
+
+  //set items to sessionStorage
+  localStorage.setItem("taxRate", taxRate);
+  localStorage.setItem("shippingPrice", shippingPrice);
+  localStorage.setItem("shippingFreePrice", shippingFreePrice);
+});
