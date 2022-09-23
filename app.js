57bc9b1a33b6916fa85e8b6bb22391b323d14048
adem kalfalı <ademkalfa9@gmail.com>
2022-09-23 15:13:35 +0300

    fa-minus_fa-plus

--- app.js
+++ app.js
@@ -13,3 +13,25 @@ window.addEventListener("load", () => {
   localStorage.setItem("shippingPrice", shippingPrice);
   localStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
+
+const productsDiv = document.querySelector(".products");
+productsDiv.addEventListener("click", (event) => {
+  if (event.target.className == "fa-solid fa-minus") {
+    // console.log("minus btn is clicked!");
+    if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+      event.target.parentElement.querySelector(".quantity").innerText--;
+    } else {
+      if (confirm("Product will be removed???")) {
+        //remove
+        event.target.parentElement.parentElement.parentElement.remove();
+      }
+    }
+  } else if (event.target.classList.contains("fa-plus")) {
+    // console.log("plus btn is clicked!");
+    event.target.previousElementSibling.innerText++;
+  } else if (event.target.className == "remove-product") {
+    // console.log("remove btn is clicked!");
+  } else {
+    // console.log("other element is clicked!");
+  }
+});
